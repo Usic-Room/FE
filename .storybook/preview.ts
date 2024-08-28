f7e501c3cddfce13fa72d651ffb0af2b588a36fc
yemin4242 <whereismysejong>
2024-08-28 21:06:44 +0900

    Test: Feature Storybook and make temp components

---
+++ .storybook/preview.ts
@@ -0,0 +1,24 @@
+import type { Preview } from "@storybook/react";
+import "@/app/globals.css";
+import { initialize, mswLoader } from "msw-storybook-addon";
+
+initialize({ onUnhandledRequest: "warn" });
+
+const preview: Preview = {
+  loaders: [mswLoader],
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+
+  tags: ["autodocs"],
+};
+
+export default preview;
