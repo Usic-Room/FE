3f6fa702ce6b02802b14d760bd16468b20806e4b
yemin4242 <whereismysejong>
2024-11-06 03:23:56 +0900

    Feat: Search page w/o query parameter with recommend component

--- src/app/(main)/home/search/page.tsx
+++ src/app/(main)/home/search/page.tsx
@@ -14,41 +14,54 @@ import LatestSongIcon from "@/public/images/latest-song.svg";
 
 interface GenreCardProps {
   title: string;
-  Icon: React.ElementType;
+  Image: React.ElementType;
+  bgColor: string;
 }
 
-const GenreCard = ({ title, Icon }: GenreCardProps) => {
+const GenreCard = ({ title, Image, bgColor }: GenreCardProps) => {
   return (
-    <div className="relative flex flex-col items-center justify-center w-full h-32 sm:h-52 bg-gray-700 rounded-md overflow-hidden text-white p-2">
-      <Icon className="relative inset-0 w-full h-full object-cover opacity-80" />
-      <div className="relative z-10 text-center text-lg font-semibold">
+    <div
+      className={`relative flex items-start justify-start w-full h-full min-w-[120px] min-h-[120px] sm:aspect-square overflow-hidden text-white rounded-xl ${bgColor}`}
+    >
+      <div className="absolute top-4 left-4 z-10 text-left text-lg font-semibold">
         {title}
       </div>
+      <Image className="absolute right-[-16px] bottom-[-16px] w-28 h-28 transform rotate-[25deg] opacity-90" />
     </div>
   );
 };
 
 export default function SearchPage() {
   const genres = [
-    { title: "이벤트", Icon: EventIcon },
-    { title: "최신 음악", Icon: LatestSongIcon },
-    { title: "팝", Icon: PopIcon },
-    { title: "차트", Icon: ChartIcon },
-    { title: "집중", Icon: FocusIcon },
-    { title: "R&B", Icon: RnBIcon },
-    { title: "가요", Icon: KoreanMusicIcon },
-    { title: "케이팝", Icon: KPopIcon },
-    { title: "재즈", Icon: JazzIcon },
-    { title: "여름", Icon: SummerIcon },
+    { title: "이벤트", Image: EventIcon, bgColor: "bg-green-27856A" },
+    { title: "최신 음악", Image: LatestSongIcon, bgColor: "bg-blue-283EA3" },
+    { title: "팝", Image: PopIcon, bgColor: "bg-blue-1E3264" },
+    { title: "차트", Image: ChartIcon, bgColor: "bg-purple-8D67AB" },
+    { title: "집중", Image: FocusIcon, bgColor: "bg-red-E8115B" },
+    { title: "R&B", Image: RnBIcon, bgColor: "bg-purple-8D67AB" },
+    { title: "가요", Image: KoreanMusicIcon, bgColor: "bg-blue-7358FF" },
+    { title: "케이팝", Image: KPopIcon, bgColor: "bg-purple-8D67AB" },
+    { title: "재즈", Image: JazzIcon, bgColor: "bg-pink-F037A5" },
+    { title: "여름", Image: SummerIcon, bgColor: "bg-blue-9CF0E1" },
   ];
 
   return (
-    <div className="flex flex-col h-full bg-black-121212 text-white p-4">
-      <h1 className="text-xl font-bold mb-4">장르 리스트</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {genres.map((genre) => (
-          <GenreCard key={genre.title} title={genre.title} Icon={genre.Icon} />
-        ))}
+    <div className="flex flex-col h-full w-full bg-black-121212 text-white pl-2">
+      <h1 className="text-xl font-bold mb-4 mt-10">모두 둘러보기</h1>
+      <div className="">
+        <div
+          style={{ gap: "2rem 1rem" }}
+          className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 mx-auto"
+        >
+          {genres.map((genre) => (
+            <GenreCard
+              key={genre.title}
+              title={genre.title}
+              Image={genre.Image}
+              bgColor={genre.bgColor}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
