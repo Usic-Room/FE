e2f9d08230526f7cdc51bbfe786da1130cc5266d
Misung Kim <misung178@gmail.com>
2024-11-14 17:12:09 +0900

    feature: 새로운 로그인 방식 시도(API 미사용)

--- src/app/(auth)/oauth/page.tsx
+++ src/app/(auth)/oauth/page.tsx
@@ -1,52 +1,94 @@
-import { HeaderSpotifyLogo } from "@/app/_components/(auth)/register/serverComponents";
+"use client";
 
+import { HeaderSpotifyLogo } from "@/app/_components/(auth)/register/serverComponents";
 import { OAuthLodingAnimation } from "@/app/_components/(auth)/oauth/clientComponents";
 import { OAuthPageDescription } from "@/app/_components/(auth)/oauth/serverComponents";
 import { Metadata } from "next";
 import { useState, useEffect, Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { sendCode } from "./api/router";
+import Link from "next/link";
 
-export const metadata: Metadata = {
-  title: "OAuth",
-};
+// export const metadata: Metadata = {
+//   title: "OAuth",
+// };
 
 export default function Page() {
-  const searchParams = useSearchParams();
-  const authorizationCode = searchParams.get("code");
-  const [data, setData] = useState(null);
-  const [error, setError] = useState<string | null>(null);
-  const [loading, setLoading] = useState(true);
   const router = useRouter();
+  const searchParams = useSearchParams();
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    const fetchData = async () => {
-      if (authorizationCode) {
-        try {
-          const result = await sendCode(authorizationCode);
-          console.log(result);
-          setData(result);
-          setLoading(false);
-          if (result) {
-            router.push("/");
-          }
-        } catch (err) {
-          setError(err.message);
-          setLoading(false);
-        }
-      }
-    };
+    const queryError = searchParams.get("error");
+    if (queryError) {
+      setError(decodeURIComponent(queryError));
+    } else {
+      router.push("/not-found");
+    }
+  }, [searchParams]);
 
-    fetchData();
-  }, [authorizationCode, router]);
+  if (!error) {
+    return null;
+  }
 
   return (
-    <Suspense fallback={<OAuthLodingAnimation />}>
-      <div className="flex flex-col items-center max-w-md mx-auto text-center bg-white min-h-screen font-bold">
-        <HeaderSpotifyLogo />
-        {/* <OAuthLodingAnimation /> */}
-        <OAuthPageDescription />
+    <div className="bg-black min-h-screen flex flex-col gap-5 items-center justify-center text-white">
+      <p className="font-bold text-2xl p-3">
+        에러코드 {error}. 오류가 발생했습니다.
+      </p>
+      <p className="font-thin text-sm">다시 시도해주세요.</p>
+      <div className="flex gap-5 flex-row">
+        <Link href="/">
+          <button className="py-4 px-8 rounded-full bg-white text-black p-4 font-bold">
+            홈
+          </button>
+        </Link>
+        <Link href="/login">
+          <button className="py-4 px-8 rounded-full bg-white text-black p-4 font-bold">
+            로그인
+          </button>
+        </Link>
       </div>
-    </Suspense>
+    </div>
   );
 }
+
+// export default function Page() {
+//   const searchParams = useSearchParams();
+//   const authorizationCode = searchParams.get("code");
+//   const [data, setData] = useState(null);
+//   const [error, setError] = useState<string | null>(null);
+//   const [loading, setLoading] = useState(true);
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       if (authorizationCode) {
+//         try {
+//           const result = await sendCode(authorizationCode);
+//           console.log(result);
+//           setData(result);
+//           setLoading(false);
+//           if (result) {
+//             router.push("/");
+//           }
+//         } catch (err) {
+//           setError(err.message);
+//           setLoading(false);
+//         }
+//       }
+//     };
+
+//     fetchData();
+//   }, [authorizationCode, router]);
+
+//   return (
+//     <Suspense fallback={<OAuthLodingAnimation />}>
+//       <div className="flex flex-col items-center max-w-md mx-auto text-center bg-white min-h-screen font-bold">
+//         <HeaderSpotifyLogo />
+//         {/* <OAuthLodingAnimation /> */}
+//         <OAuthPageDescription />
+//       </div>
+//     </Suspense>
+//   );
+// }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,21 +3,21 @@ import Link from "next/link";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-	title: "Not found",
+  title: "Not found",
 };
 
 const NotFound: NextPage = () => {
-	return (
-		<div className="bg-black min-h-screen flex flex-col gap-5 items-center justify-center text-white">
-			<p className="font-bold text-2xl p-3">페이지를 찾을 수 없음</p>
-			<p className="font-thin text-sm">원하는 페이지를 찾지 못했습니다.</p>
-			<Link href="/home">
-				<button className="py-4 px-8 rounded-full bg-white text-black p-4 font-bold">
-					홈
-				</button>
-			</Link>
-		</div>
-	);
+  return (
+    <div className="bg-black min-h-screen flex flex-col gap-5 items-center justify-center text-white">
+      <p className="font-bold text-2xl p-3">페이지를 찾을 수 없음</p>
+      <p className="font-thin text-sm">원하는 페이지를 찾지 못했습니다.</p>
+      <Link href="/">
+        <button className="py-4 px-8 rounded-full bg-white text-black p-4 font-bold">
+          홈
+        </button>
+      </Link>
+    </div>
+  );
 };
 export default NotFound;
 
